a0bc90308afbcd080b95b4e6ce761942296beceb
@vijayapj <vijay.apj@gmail.com>
2022-07-16 14:23:11 +0530

    fast api changes

--- app/main.py
+++ app/main.py
@@ -5,27 +5,31 @@ app = FastAPI()
 
 @app.get("/hello")
 def root():
-    return {"message": "Fast API in Python"}
+    return {"message": "Fast API in Python 123"}
 
+#file: UploadFile = File(description="A file read as UploadFile")
 
 @app.post('/uploadfile')
 def _file_upload(my_file: UploadFile = File(...)):
   print('hello')
-  cv2_imshow(my_file)
+  //1. read the image-file and view
+  //2. call the pre-trained model function 
+  //3. return the taged image
   return {
-      "name": my_file,
-      "first": first
+      "name"
       }
 
-@app.post("/files/")
+@app.post("/files")
 async def create_file(file: bytes = File(...)):
   print('hello, inside create_file')
   return {"file_size": len(file)}
 
 
 
-@app.post("/uploadfile2/")
-async def create_upload_file(file: UploadFile):
+@app.post("/uploadfile2")
+async def create_upload_file(image: UploadFile):
   print('hello, inside create_upload_file')  
-  return {"filename": file}
+  return {"filename"}
+
+
 
