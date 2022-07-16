949ff09af40abe1f7905669b348f19fd96edd0d0
@vijayapj <vijay.apj@gmail.com>
2022-07-16 08:47:01 +0530

    updated for file upload

--- app/main.py
+++ app/main.py
@@ -1,10 +1,31 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/hello")
 def root():
     return {"message": "Fast API in Python"}
 
+
+@app.post('/uploadfile')
+def _file_upload(my_file: UploadFile = File(...)):
+  print('hello')
+  cv2_imshow(my_file)
+  return {
+      "name": my_file,
+      "first": first
+      }
+
+@app.post("/files/")
+async def create_file(file: bytes = File(...)):
+  print('hello, inside create_file')
+  return {"file_size": len(file)}
+
+
+
+@app.post("/uploadfile2/")
+async def create_upload_file(file: UploadFile):
+  print('hello, inside create_upload_file')  
+  return {"filename": file}
 
