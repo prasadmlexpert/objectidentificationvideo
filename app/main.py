6c80aab08f8483d823a8aa71a89cb7a29b3287e0
@vijayapj <vijay.apj@gmail.com>
2022-07-24 13:03:56 +0530

    added api file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException, File, UploadFile, Form
 from google.colab.patches import cv2_imshow
+from pretrained import api
 
 app = FastAPI()
 
@@ -13,17 +14,17 @@ def root():
 @app.post('/uploadfile')
 def _file_upload(my_file: UploadFile = File(...)):
   print('hello')
-  //1. read the image-file and view
-  img_to_detect = cv2.imread('images/10k/train/004071a4-049be89b.jpg')
+  #1. read the image-file and view
+  img_to_detect = my_file #cv2.imread('images/10k/train/004071a4-049be89b.jpg')
   img_height = img_to_detect.shape[0]
   img_width = img_to_detect.shape[1]
   img_blob = cv2.dnn.blobFromImage(img_to_detect, 0.003922, (416, 416), swapRB=True, crop=False)
 
-  //2. call the pre-trained model function
-  calculateConfidence(img_blob) 
+  #2. call the pre-trained model function
+  api.calculateConfidence(img_blob) 
   
-  //3. return the taged image
-  tagged_img = predictions()
+  #3. return the taged image
+  tagged_img = api.predictions()
 
   cv2_imshow(tagged_img)
   cv2.imwrite("results.jpg", tagged_img)
@@ -38,11 +39,9 @@ async def create_file(file: bytes = File(...)):
   return {"file_size": len(file)}
 
 
-
 @app.post("/uploadfile2")
 async def create_upload_file(image: UploadFile):
   print('hello, inside create_upload_file')  
   return {"filename"}
-
 
 
